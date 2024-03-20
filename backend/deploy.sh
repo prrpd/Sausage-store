91696ca95d9e1e936f4ec411ed357849c1716909
std-027-58 <std-027-58@example.com>
2024-03-20 21:29:30 +0300

    added chown

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,6 +7,7 @@ sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+sudo chown -R backend:backend /opt/sausage-store/bin/
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
