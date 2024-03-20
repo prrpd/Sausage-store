057f1f16b6d56a51274d7ffea617a31fe7fda9e2
std-027-58 <std-027-58@example.com>
2024-03-20 21:45:16 +0300

    moved chown back

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -8,7 +8,7 @@ sudo rm -rf /opt/sausage-store/static/*||true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o front.tar.gz ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo tar -xvzf front.tar.gz
 sudo mv -f frontend/* /opt/sausage-store/static/||true #"<...>||true" говорит, если команда обвалится — продолжай
-#sudo chown -R frontend:frontend /opt/sausage-store/static/
+sudo chown -R frontend:frontend /opt/sausage-store/static/
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
