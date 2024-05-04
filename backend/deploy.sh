e8eaee60e69049452eddd7daadf66b729470e5e8
std-027-58 <std-027-58@example.com>
2024-05-04 13:59:46 +0300

    changed unit file

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,6 +8,14 @@ sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
 sudo chown -R backend:backend /opt/sausage-store/bin/
+
+#setting env variable to env file
+echo "PSQL_USER=${PSQL_USER}" > /etc/default/sausage-store-backend
+echo "PSQL_PASSWORD=${PSQL_PASSWORD}" >> /etc/default/sausage-store-backend
+echo "PSQL_HOST=${PSQL_HOST}" >> /etc/default/sausage-store-backend
+echo "PSQL_DBNAME=${PSQL_DBNAME}" >> /etc/default/sausage-store-backend
+echo "PSQL_PORT=${PSQL_PORT}" >> /etc/default/sausage-store-backend
+
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
