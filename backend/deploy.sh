b1c6be719796021111ad073787fa3790fb0c8497
std-027-58 std-027-58 <std-027-58@praktikum-services.ru>
2024-05-07 15:02:36 +0000

    Mongo

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,21 +3,25 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf back.service /etc/systemd/system/back.service
+sudo cp -rf sausage-store-backend /etc/default/sausage-store-backend
 sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
 sudo chown -R backend:backend /opt/sausage-store/bin/
 
-#setting env variable to env file
-sudo bash -c "echo "PSQL_USER=${PSQL_USER}" > /etc/default/sausage-store-backend"
-sudo bash -c "echo "PSQL_PASSWORD=${PSQL_PASSWORD}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "PSQL_HOST=${PSQL_HOST}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "PSQL_DBNAME=${PSQL_DBNAME}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "PSQL_PORT=${PSQL_PORT}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "SPRING_DATASOURCE_USERNAME=${PSQL_USER}" >> /etc/default/sausage-store-backend"
-sudo bash -c "echo "SPRING_DATASOURCE_PASSWORD=${PSQL_PASSWORD}" >> /etc/default/sausage-store-backend"
+#postgres
+sudo mkdir -p /home/backend/.postgresql && \
+sudo wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O /home/backend/.postgresql/root.crt && \
+sudo chmod 0600 /home/backend/.postgresql/root.crt
+#mongo
+sudo wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O YandexInternalRootCA.crt
+sudo keytool -importcert \
+             -file YandexInternalRootCA.crt \
+             -alias yandex-$RANDOM \
+             -cacerts \
+             -storepass ${KEYSTORE_PASS} \
+             -noprompt
 
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
