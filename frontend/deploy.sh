bded37546e1314bb1df0c017edbcd48ffac46043
std-027-58 <std-027-58@example.com>
2024-05-11 16:38:06 +0300

    changed gitlabCI

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
-sudo docker run -d --name sausage-frontend \
+sudo docker run --rm -d --name sausage-frontend \
      --network=sausage_network \
      -p 8080:80 \
      --restart=on-failure:10 \
