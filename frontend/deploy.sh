36ad32b796a67773b02326491d0481360a1a5b24
std-027-58 <std-027-58@example.com>
2024-05-11 16:27:51 +0300

    changed gitlabCI

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,5 +7,5 @@ sudo docker run -d --name sausage-frontend \
      --network=sausage_network \
      -p 8080:80 \
      --restart=on-failure:10 \
-     -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
+     -v ${NGINX_CONF}:/etc/nginx/conf.d/default.conf \
      "${CI_REGISTRY_IMAGE}"/sausage-frontend:${VERSION}
