15c8770bf7d658e41b91e3cf407742b917e79660
std-027-58 <std-027-58@example.com>
2024-06-04 21:24:17 +0300

    blue-green

--- backend/deploy-bg.sh
+++ backend/deploy-bg.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 set -xe
-if CONT=$(docker ps -qf name=backend-blue); then
+if CONT=$(docker --context remote ps -qf name=backend-blue); then
     NEW_ENV="backend-green"
     CUR_ENV="backend-blue"
-elif CONT=$(docker ps -qf name=backend-green); then
+elif CONT=$(docker --context remote ps -qf name=backend-green); then
     NEW_ENV="backend-blue"
     CUR_ENV="backend-green"
 else
     NEW_ENV="backend-blue"
     CUR_ENV=""
 fi
-
+pwd
 echo "Starting "$NEW_ENV" container"
 docker --context remote compose --env-file deploy.env up $NEW_ENV -d --pull "always" --force-recreate
 
 echo "Waiting..."
 sleep 35s
 
-test=$(docker inspect --context remote --format='{{.State.Health.Status}}' $CONT)
+test=$(docker --context remote inspect --format='{{.State.Health.Status}}' $CONT)
 if [ $test = "healthy" ]; then
     if [ -n "$CUR_ENV" ]; then
         echo "Stopping "$CUR_ENV" container"
         docker --context remote compose down $CUR_ENV
     fi
+else
+    echo "Container $CONT is not healthy"    
 fi
